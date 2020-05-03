deaf59aeef2a251ec2e3e2f917c5b674f0208ce1
ogii <ordinallx@gmail.com>
2020-05-03 19:25:54 +0900

    Add log in functionality

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const User = require('../models/User');
 
@@ -50,7 +52,21 @@ router.post(
 
       await user.save();
 
-      res.send('User saved');
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000
+        }, (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
 
     } catch (err) {
       console.error(err.message);
