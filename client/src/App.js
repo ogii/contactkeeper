e2d11062ea12a0757fbcc6992a5e1bb6277db3e9
ogii <ordinallx@gmail.com>
2020-05-05 14:01:22 +0900

    Added basic react components

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React, { Fragment } from 'react';
+import Navbar from './components/layout/Navbar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import './App.css';
+
+const App = () => {
+  return (
+    <Router>
+      <Fragment className="App">
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/about' component={About} />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
+
+  );
+};
+
+export default App;
