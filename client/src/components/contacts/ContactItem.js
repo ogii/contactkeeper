792160d4d81af455ff4247a9421d8cb60087f131
ogii <ordinallx@gmail.com>
2020-05-05 18:42:59 +0900

    Update edit contact functionality

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -1,15 +1,17 @@
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import ContactContext from '../../context/contact/contactContext';
+import { SET_CURRENT } from '../../context/types';
 
 const ContactItem = ({ contact }) => {
   const contactContext = useContext(ContactContext);
-  const { deleteContact } = contactContext;
+  const { deleteContact, setCurrent, clearCurrent } = contactContext;
 
   const { id, name, email, phone, type } = contact;
 
   const onDelete = () => {
     deleteContact(id);
+    clearCurrent();
   };
 
   return (
@@ -39,7 +41,12 @@ const ContactItem = ({ contact }) => {
         )}
       </ul>
       <p>
-        <button className='btn btn-dark btn-sm'>Edit</button>
+        <button
+          className='btn btn-dark btn-sm'
+          onClick={() => setCurrent(contact)}
+        >
+          Edit
+        </button>
         <button className='btn btn-danger btn-sm' onClick={onDelete}>
           Delete
         </button>
--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -9,10 +9,10 @@ import {
   CLEAR_CURRENT,
   UPDATE_CONTACT,
   FILTER_CONTACTS,
-  CLEAR_FILTER
+  CLEAR_FILTER,
 } from '../types';
 
-const ContactState = props => {
+const ContactState = (props) => {
   const initialState = {
     contacts: [
       {
@@ -20,43 +20,53 @@ const ContactState = props => {
         name: 'Jill Johnson',
         email: 'jill@gmail.com',
         phone: '111-111-1111',
-        type: 'personal'
+        type: 'personal',
       },
       {
         id: 2,
         name: 'Sara Watson',
         email: 'sara@gmail.com',
         phone: '222-222-2222',
-        type: 'personal'
+        type: 'personal',
       },
       {
         id: 3,
         name: 'Harry White',
         email: 'harry@gmail.com',
         phone: '333-333-3333',
-        type: 'professional'
-      }
-    ]
+        type: 'professional',
+      },
+    ],
+    current: null,
   };
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
 
   // Add Contact
-  const addContact = contact => {
+  const addContact = (contact) => {
     contact.id = uuidv4();
     dispatch({ type: ADD_CONTACT, payload: contact });
   };
 
   // Delete Contact
-  const deleteContact = id => {
+  const deleteContact = (id) => {
     dispatch({ type: DELETE_CONTACT, payload: id });
   };
 
   // Set Current Contact
+  const setCurrent = (contact) => {
+    dispatch({ type: SET_CURRENT, payload: contact });
+  };
 
   // Clear Current Contact
+  const clearCurrent = () => {
+    dispatch({ type: CLEAR_CURRENT });
+  };
 
   // Update Contact
+  const updateContact = (contact) => {
+    dispatch({ type: UPDATE_CONTACT, payload: contact });
+  };
 
   // Filter Contacts
 
@@ -66,13 +76,17 @@ const ContactState = props => {
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
+        current: state.current,
         addContact,
-        deleteContact
-      }}>
+        deleteContact,
+        setCurrent,
+        clearCurrent,
+        updateContact,
+      }}
+    >
       {props.children}
     </ContactContext.Provider>
   );
-
 };
 
 export default ContactState;
