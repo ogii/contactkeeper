7a706828f44abf7abbdd390546b5f5421f1705f4
ogii <ordinallx@gmail.com>
2020-05-06 14:27:08 +0900

    Added filter contact component

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -38,6 +38,7 @@ const ContactState = (props) => {
       },
     ],
     current: null,
+    filtered: null,
   };
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
@@ -69,14 +70,23 @@ const ContactState = (props) => {
   };
 
   // Filter Contacts
+  const filterContacts = (text) => {
+    dispatch({ type: FILTER_CONTACTS, payload: text });
+  };
 
   // Clear Filter
+  const clearFilter = () => {
+    dispatch({ type: CLEAR_FILTER });
+  };
 
   return (
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
         current: state.current,
+        filtered: state.filtered,
+        filterContacts,
+        clearFilter,
         addContact,
         deleteContact,
         setCurrent,
